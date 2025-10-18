248d6534666c0a93b86d6bef65704d668edd0e59
bme4248-byte <bme4248@gmail.com>
2025-10-18 17:21:55 -0400

    ya tengo template hecho

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+        return view('template');
+});
+
+Route::view('/panel', 'panel.index')->name('panel');
+
+Route::get('/login', function() {
+        return view('auth.login');
 });
